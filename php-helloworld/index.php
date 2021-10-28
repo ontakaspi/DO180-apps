5da7495a4b72e2c898f1f13259dc764279b8bb76
Muhammad Kasfi <49944982+ontakaspi@users.noreply.github.com>
2021-10-28 15:58:10 +0700

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+if(getenv("DECODER_RING")){
+print "the app env is configured";
+}else{
+  print "sorry, application is not configured correctly"
 ?>
